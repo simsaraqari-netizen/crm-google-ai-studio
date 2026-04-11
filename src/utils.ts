ed5ce77c658f4450a823ea73276f9e740b6f0889
Mostafa <mostafasoliman550@gmail.com>
2026-04-11 05:04:51 +0300

    تعديلات

--- src/utils.ts
+++ src/utils.ts
@@ -253,13 +253,13 @@ export const formatDateTime = (date: any) => {
     const d = new Date(date);
     if (isNaN(d.getTime())) return '';
     
-    const day = String(d.getDate()).padStart(2, '0');
-    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = d.getDate();
+    const month = d.getMonth() + 1;
     const year = d.getFullYear();
     const hours = String(d.getHours()).padStart(2, '0');
     const minutes = String(d.getMinutes()).padStart(2, '0');
     
-    return `${hours}:${minutes} | ${day}/${month}/${year}`;
+    return `${hours}:${minutes} ${day}-${month}-${year}`;
   } catch (e) {
     return '';
   }
